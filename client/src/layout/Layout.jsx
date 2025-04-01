097d02b25d37d5dc9d742389c3d757bf2ad81c95
Ma-Eum <cl.gachelin@gmail.com>
2025-04-01 15:23:11 +0200

    feat: création de la page d'accueil avec hero et fonctionnalités

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 // src/layout/Layout.jsx
 import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../layout/Header'
+import Footer from '../layout/Footer'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
