338476eb54d4af420a0ced4e70e09f3cffcfdf66
Ma-Eum <cl.gachelin@gmail.com>
2026-03-24 13:22:18 +0100

    fix(backend) : correction de la connexion MongoDB et remise en fonctionnement de l’API

--- backend/server/database/connection.js
+++ backend/server/database/connection.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
+
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/argentBankDB'
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    console.log('Tentative de connexion à :', databaseUrl)
+    await mongoose.connect(databaseUrl)
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    throw error
   }
 }
